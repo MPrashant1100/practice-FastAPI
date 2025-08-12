156e3b185f7f1ed24ae81e0507382154ca226870
mprashant1100 <prashantmishra1100@gmail.com>
2025-08-12 23:58:34 +0530

    simple user model created using pydnatic

--- project1-userAuth/main.py
+++ project1-userAuth/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -8,3 +9,15 @@ def home_root():
     return {
         "Hello, This is Home page"
         }
+
+# Pydantic models (schemas)
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=6)
+
+class User(BaseModel):
+    username: str
+
+class Token: 
+    access_Token: str
+    token_type: str
